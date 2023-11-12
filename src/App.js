defb645eab198d5ebb32cbb55401119db27610d8
JoceFoo <jlyjoce@gmail.com>
2023-11-12 14:32:36 +0800

    add routing

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
-import React, { Component } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
-import Container from 'react-bootstrap/Container';
-import InvoiceForm from './components/InvoiceForm';
+import React, { Component } from "react";
+import Container from "react-bootstrap/Container";
+import { Routes, Route} from 'react-router-dom';
+
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import InvoiceForm from "./components/InvoiceForm";
 
 class App extends Component {
   render() {
-  return (
-    <div className="App d-flex flex-column align-items-center justify-content-center w-100">
-      <Container>
-        <InvoiceForm/>
-      </Container>
-    </div>
-  );
-}}
+    return (
+      <div className="App d-flex flex-column align-items-center justify-content-center w-100">
+        <Container>
+          <Routes>
+            <Route path="/form" element={<InvoiceForm />} />
+          </Routes>
+        </Container>
+      </div>
+    );
+  }
+}
 
 export default App;
