9196616a0337397bbfb0218614cee85acc148df2
Lee Kai Yang <kaiyang01007@gmail.com>
2023-12-08 00:59:49 +0800

    feat: Implemented Invoice Generated History

---
+++ src/components/HistoryButton.js
@@ -0,0 +1,24 @@
+// HistoryButton.js
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { FaHome } from "react-icons/fa";
+
+const HistoryButton = () => {
+  const navigate = useNavigate();
+
+
+  return (
+    <div>
+
+        <button
+          onClick={() => navigate("/history")}
+          className="btn btn-secondary rounded"
+          style={{ marginRight: "10px" }}
+        >
+          <FaHome /> Invoice History
+        </button>
+    </div>
+  );
+};
+
+export default HistoryButton;
