5df658d56bb177cf96901f425a489628fe03ae60
lemonmel <u2005349@siswa.um.edu.my>
2023-12-17 19:39:34 +0800

    Send Invoice function updated

--- src/components/InvoiceModal.js
+++ src/components/InvoiceModal.js
@@ -5,57 +5,59 @@ import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
 import Table from 'react-bootstrap/Table';
 import Modal from 'react-bootstrap/Modal';
-import { BiPaperPlane, BiCloudDownload } from "react-icons/bi";
+import { BiPaperPlane, BiCloudDownload, BiPlusCircle } from "react-icons/bi";
 import html2canvas from 'html2canvas';
 import jsPDF from 'jspdf'
 import { ref, uploadString, getDownloadURL  } from 'firebase/storage';
-import { getFirestore, collection, addDoc, Timestamp } from 'firebase/firestore';
+import { collection, addDoc, Timestamp, doc, getDoc } from 'firebase/firestore';
 import { db, auth, storage } from "../firebase";
+import emailjs from '@emailjs/browser';
 
 
 // var userEmail = auth.currentUser ? auth.currentUser.email : 'GUEST';
 
 function GenerateInvoice(billTo) {
+  return new Promise((resolve, reject) => {
+    const pdf = new jsPDF({
+      orientation: 'portrait',
+      unit: 'pt',
+      format: [612, 792],
+    });
 
-  const pdf = new jsPDF({
-    orientation: 'portrait',
-    unit: 'pt',
-    format: [612, 792],
+    const source = document.getElementById('invoiceCapture');
+    
+    // Determine the actual height of the content
+    const contentHeight = source.offsetHeight;
+
+    // Set a ratio to scale content to fit within the PDF page
+    const scaleFactor = 342 / contentHeight; // Adjust 792 to the desired page height
+    let pdfID = null;
+    // Generate PDF with scaled content
+    pdf.html(source, {
+      callback: async function (pdf) {
+        // Assuming 'pdf' is the generated PDF document using jsPDF
+        const totalPages = pdf.internal.getNumberOfPages();
+
+        // Remove extra pages after the first page
+        for (let i = totalPages; i > 1; i--) {
+          pdf.deletePage(i);
+        }
+        
+        pdf.save('invoice-001.pdf');
+
+        //save into firebase
+        const pdfUrl = await savePdfToStorage(pdf);
+        // alert(pdfUrl)
+        pdfID = await savePdfUrlToFirestore(pdfUrl, billTo);
+        resolve(pdfID);
+      },
+      x: 0,
+      y: 0,
+      html2canvas: {
+        scale: scaleFactor,
+      },
+    });
   });
-
-  const source = document.getElementById('invoiceCapture');
-  
-  // Determine the actual height of the content
-  const contentHeight = source.offsetHeight;
-
-  // Set a ratio to scale content to fit within the PDF page
-  const scaleFactor = 342 / contentHeight; // Adjust 792 to the desired page height
-
-  // Generate PDF with scaled content
-  pdf.html(source, {
-    callback: async function (pdf) {
-      // Assuming 'pdf' is the generated PDF document using jsPDF
-      const totalPages = pdf.internal.getNumberOfPages();
-
-      // Remove extra pages after the first page
-      for (let i = totalPages; i > 1; i--) {
-        pdf.deletePage(i);
-      }
-      
-      pdf.save('invoice-001.pdf');
-
-      //save into firebase
-      const pdfUrl = await savePdfToStorage(pdf);
-      // alert(pdfUrl)
-      await savePdfUrlToFirestore(pdfUrl, billTo);
-    },
-    x: 0,
-    y: 0,
-    html2canvas: {
-      scale: scaleFactor,
-    },
-  });
-
 }
 
 async function savePdfToStorage(pdf) {
@@ -88,13 +90,13 @@ async function savePdfUrlToFirestore(pdfUrl, billTo) {
   var myTimestamp = Timestamp.fromDate(new Date());
   
   try {
-    await addDoc(invoicesCollectionRef, { 
+    const docRef = await addDoc(invoicesCollectionRef, { 
       email: auth.currentUser ? auth.currentUser.email : 'GUEST',
       receiver: billTo,
       time: myTimestamp,
       link: pdfUrl 
     });
-    
+    return docRef.id;
   } catch (error) {
     console.error(error)
   }
@@ -117,10 +119,61 @@ async function savePdfUrlToFirestore(pdfUrl, billTo) {
 //   });
 // }
 
+async function SendInvoice(id, billFrom, billTo, billFromEmail, billToEmail){
+  try {
+    const docRef = doc(db, "invoice", id);
+    const docSnap = await getDoc(docRef);
+    const pdfUrl = docSnap.data().link;
+
+    emailjs.send('service_sk0273i', 'template_edd8fcq', {
+      billFrom: billFrom,
+      billTo: billTo,
+      billFromEmail: billFromEmail,
+      billToEmail: billToEmail,
+      message: pdfUrl,
+    }, 'a-mqEqfLpAycZy9yF')
+      .then((response) => {
+        alert('Email sent successfully!');
+      })
+      .catch((error) => {
+        alert('Email error: '+error);
+      });
+  } catch (error) {
+    alert('PDF link is generating, click again to send the email');
+    console.log(error)
+  }
+}
+
 class InvoiceModal extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      invoiceGenerated: false, // Track if invoice has been generated and stored
+      currentPdfID: null
+    };
   }
+
+  async CheckGenerate(billTo) {
+    if (!this.state.invoiceGenerated) {
+      // Set invoiceGenerated state to true to prevent further duplication
+      this.setState({ invoiceGenerated: true });
+      await GenerateInvoice(billTo).then(pdfID => {
+        this.setState({currentPdfID: pdfID})
+        console.log(this.state.currentPdfID+' , generated:'+pdfID);
+      });
+    } else {
+      alert("Invoice already generated and saved. Click on 'Create New' to create a new invoice!");
+    }
+  }
+
+  async CheckSend(billFromEmail, billToEmail, billFrom, billTo) {
+    if (!this.state.invoiceGenerated) {
+      await this.CheckGenerate(billTo);
+    } 
+    alert('Sending your email...');
+    SendInvoice(this.state.currentPdfID, billFrom, billTo, billFromEmail, billToEmail);
+  }
+
   render() {
     return(
       <div>
@@ -130,7 +183,7 @@ class InvoiceModal extends React.Component {
               <div className="w-100">
                 <h4 className="fw-bold my-2">{this.props.info.billFrom||'John Uberbacher'}</h4>
                 <h6 className="fw-bold text-secondary mb-1">
-                  Invoice #: {this.props.info.invoiceNumber||''}
+                  Invoice 1: {this.props.info.invoiceNumber||''}
                 </h6>
               </div>
               <div className="text-end ms-4">
@@ -224,15 +277,21 @@ class InvoiceModal extends React.Component {
           </div>
           <div className="pb-4 px-4">
             <Row>
-              <Col md={6}>
-                <Button variant="primary" className="d-block w-100" onClick={() => GenerateInvoice(this.props.info.billTo)}>
+              <Col md={4}>
+                <Button variant="primary" className="d-block w-100" onClick={() => this.CheckSend(this.props.info.billFromEmail, this.props.info.billToEmail, this.props.info.billFrom, this.props.info.billTo)}>
                   <BiPaperPlane style={{width: '15px', height: '15px', marginTop: '-3px'}} className="me-2"/>Send Invoice
                 </Button>
               </Col>
-              <Col md={6}>
-                <Button variant="outline-primary" className="d-block w-100 mt-3 mt-md-0" onClick={() => GenerateInvoice(this.props.info.billTo)}>
+              <Col md={4}>
+                <Button variant="outline-primary" className="d-block w-100 mt-3 mt-md-0" onClick={() => this.CheckGenerate(this.props.info.billTo)}>
                   <BiCloudDownload style={{width: '16px', height: '16px', marginTop: '-3px'}} className="me-2"/>
                   Download Copy
+                </Button>
+              </Col>
+              <Col md={4}>
+                <Button variant="primary" className="d-block w-100 mt-3 mt-md-0" onClick={() => window.location.reload()}>
+                  <BiPlusCircle style={{width: '16px', height: '16px', marginTop: '-3px'}} className="me-2"/>
+                  Create New
                 </Button>
               </Col>
             </Row>
